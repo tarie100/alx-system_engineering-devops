f3659f8fc636e839c45adbcd95acae2a816d8895
tarie100 <tarie.school@gmail.com>
2023-10-31 12:40:02 -0700

    5

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-def match_string(input)
-  regex = /^h.n$/
-  if input.match?(regex)
-    puts "The string '#{input}' matches the regular expression!"
-  else
-    puts "The string '#{input}' does not match the regular expression."
-  end
-end
-
-# Accepting user input as argument
-argument = ARGV[0]
-
-# Calling the method with the provided argument
-match_string(argument)
+puts ARGV[0].scan(/^h.n$/).join
