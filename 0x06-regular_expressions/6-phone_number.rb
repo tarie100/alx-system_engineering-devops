66d871776be83fd47b7bfacfb62f642521f5c8b1
tarie100 <tarie.school@gmail.com>
2023-10-31 12:57:13 -0700

    6

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,2 +1,15 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/\d{10}/).join
+def match_phone_number(input)
+  regex = /^\d{10}$/
+  if input.match?(regex)
+    puts "The string '#{input}' is a valid 10-digit phone number!"
+  else
+    puts "The string '#{input}' is not a valid 10-digit phone number."
+  end
+end
+
+# Accepting user input as argument
+argument = ARGV[0]
+
+# Calling the method with the provided argument
+match_phone_number(argument)
