dd2adc9f2ab8888fa7d22ef5d0aa692fb5827c76
tarie100 <tarie.school@gmail.com>
2024-01-29 12:23:07 -0800

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-import requests
+"""script using this REST API, for a given employee ID,
+returns information about his/her TODO list progress."""
+import requests as r
+import sys
 
-def get_employee_todo_progress(employee_id):
-    base_url = 'https://api.example.com/'
-    employee_url = base_url + 'employees/' + str(employee_id)
-    todos_url = base_url + 'todos?employeeId=' + str(employee_id)
-
-    try:
-        employee_response = requests.get(employee_url)
-        todos_response = requests.get(todos_url)
-
-        if employee_response.status_code == 200 and todos_response.status_code == 200:
-            employee_data = employee_response.json()
-            todos_data = todos_response.json()
-
-            employee_name = employee_data['name']
-            total_tasks = len(todos_data)
-            done_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-            print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
-
-            for todo in todos_data:
-                if todo['completed']:
-                    print(f"\t {todo['title']}")
-
-        else:
-            print("Error: Unable to retrieve employee and/or TODO data.")
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    usr_id = r.get(url + 'users/{}'.format(sys.argv[1])).json()
+    to_do = r.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+#    print(to_do)
+    completed = [title.get("title") for title in to_do if
+                 title.get('completed') is True]
+    print(completed)
+    print("Employee {} is done with tasks({}/{}):".format(usr_id.get("name"),
+                                                          len(completed),
+                                                          len(to_do)))
+    [print("\t {}".format(title)) for title in completed]
