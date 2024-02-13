48889dba33a666c0bc5b15c0339b16895e43660c
tarie100 <tarie.school@gmail.com>
2024-02-13 12:12:19 -0800

    fix1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 
 
 import requests
+
 
 def recurse(subreddit, hot_list=[]):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
