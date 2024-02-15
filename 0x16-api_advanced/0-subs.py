7e6cb5ea0cfa0520d425219792b79de98e344fed
tarie100 <tarie.school@gmail.com>
2024-02-15 12:37:46 -0800

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,9 @@ function that queries the Reddit API and returns the number of subscribers
 import requests
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{}about.json".format(subreddit)
-    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0"}
+    url = "https://www.reddit.com/r/{}about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+            }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     
