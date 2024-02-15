ec9f011affb86386275d21ed8ada59141b664b43
tarie100 <tarie.school@gmail.com>
2024-02-15 12:13:37 -0800

    fix errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,10 @@ def search_results_redirect(query):
     url = f"https://www.reddit.com/search?q={query}"
     headers = {'User-Agent': 'Your Custom User Agent'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-
-        if response.status_code == 302 and 'Location' in response.headers:
-            return response.headers['Location']
-        else:
-            return None
-
-    except:
-        return None
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,28 +7,19 @@ function that queries the Reddit API and prints the titles of the first 10 hot p
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Your Custom User Agent'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {'limit': 10}
+    
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    
+    if response.status_code == 200:
         data = response.json()
-
-        if response.status_code == 200 and 'data' in data and 'children' in data['data']:
-            posts = data['data']['children']
-
-            for post in posts:
-                hot_list.append(post['data']['title'])
-
-            if 'after' in data['data'] and data['data']['after'] is not None:
-                after = data['data']['after']
-                return recurse(subreddit, hot_list=hot_list, after=after)
-
-            return hot_list
-
-        else:
-            return None
-
-    except:
-        return None
+        posts = data['data']['children']
+        
+        for post in posts:
+            title = post['data']['title']
+            print(title)
+    else:
+        print(None)
